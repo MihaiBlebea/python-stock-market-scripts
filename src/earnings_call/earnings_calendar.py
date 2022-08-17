5d2373fc693df7db2ddcb8cfcd85c59b054babf9
Mihai Blebea <mihaiserban.blebea@gmail.com>
2022-08-17 14:22:17 +0100

    Adds last 4 earnings to calendar

--- src/earnings_call/earnings_calendar.py
+++ src/earnings_call/earnings_calendar.py
@@ -14,21 +14,32 @@ def is_next_earning(earning: dict)-> bool:
 		and earning["epsactual"] is None\
 		and to_datetime(earning["startdatetime"]).replace(tzinfo=utc) > datetime.now().replace(tzinfo=utc)
 
+def get_last_earnings(earnings: list, current_index: int)-> list:
+	i = current_index + 1
+	return [ 
+		{
+			"eps_estimate": earning["epsestimate"],
+			"eps_actual": earning["epsactual"],
+			"diff": round(earning["epsactual"] - earning["epsestimate"], 2)
+		} for earning in earnings[i:i+4] if earning["epsestimate"] is not None and earning["epsactual"] is not None
+	]
 
 def main():
 	universe = get_sp500()
 	earnings_list = []
 	for ticker in universe:
 		earnings = get_earnings(ticker)
-		for earning in earnings:
+		for i, earning in enumerate(earnings):
 			if is_next_earning(earning):
 				earnings_list.append({
 					"company": earning["companyshortname"],
 					"symbol": earning["ticker"],
 					"datetime": earning["startdatetime"],
 					"eps_estimate": earning["epsestimate"],
+					"last_4": get_last_earnings(earnings, i)
 				})
 				break
+
 
 	calendar_list = sorted(
 		earnings_list, 
